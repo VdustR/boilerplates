e2b826384b6ec3d314cf1707f9ab9add53dd5e0c
Vi Pro <VdustR@gmail.com>
2019-03-06 22:05:00 +0800

    chore: add eslint-plugin-react-hooks

--- react/.eslintrc.js
+++ react/.eslintrc.js
@@ -1,3 +1,8 @@
 module.exports = {
   extends: ['react-app', 'prettier'],
+  plugins: ['react-hooks'],
+  rules: {
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
 };
