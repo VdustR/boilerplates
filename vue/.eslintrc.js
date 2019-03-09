0c999ccfbf0f4147e21cd57c9a80447f90dba3fe
Vi Pro <VdustR@gmail.com>
2019-03-09 17:53:44 +0800

    chore(vue): fix eslint

--- vue/.eslintrc.js
+++ vue/.eslintrc.js
@@ -3,10 +3,24 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ['plugin:vue/recommended', '@vue/prettier'],
+  extends: ['plugin:vue/recommended', 'prettier'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    /**
+     * vue/max-attributes-per-line conflicts with prettier
+     * https://github.com/prettier/eslint-plugin-prettier/issues/94#issuecomment-438781791
+     */
+    'vue/max-attributes-per-line': [
+      2,
+      {
+        singleline: 20,
+        multiline: {
+          max: 1,
+          allowFirstLine: false,
+        },
+      },
+    ],
   },
   parserOptions: {
     parser: 'babel-eslint',
