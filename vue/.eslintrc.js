c327dab675514a9aef9e5473b98866afbb88a679
Vi Pro <VdustR@gmail.com>
2019-03-12 00:28:08 +0800

    chore: disable all eslint rules which conflicts with prettier

--- vue/.eslintrc.js
+++ vue/.eslintrc.js
@@ -7,20 +7,15 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    /**
-     * vue/max-attributes-per-line conflicts with prettier
-     * https://github.com/prettier/eslint-plugin-prettier/issues/94#issuecomment-438781791
-     */
-    'vue/max-attributes-per-line': [
-      2,
-      {
-        singleline: 20,
-        multiline: {
-          max: 1,
-          allowFirstLine: false,
-        },
-      },
-    ],
+    'vue/html-closing-bracket-newline': 'off',
+    'vue/html-closing-bracket-spacing': 'off',
+    'vue/html-end-tags': 'off',
+    'vue/html-indent': 'off',
+    'vue/html-quotes': 'off',
+    'vue/html-self-closing': 'off',
+    'vue/max-attributes-per-line': 'off',
+    'vue/multiline-html-element-content-newline': 'off',
+    'vue/singleline-html-element-content-newline': 'off',
   },
   parserOptions: {
     parser: 'babel-eslint',
