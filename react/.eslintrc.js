acf65069a0dc79455100efca5c14dff5237ff43f
Vi Pro <VdustR@gmail.com>
2019-04-23 21:37:51 +0800

    build(react): upgrade modules
    create-react-app 3.0

--- react/.eslintrc.js
+++ react/.eslintrc.js
@@ -1,8 +1,3 @@
 module.exports = {
-  extends: ['react-app', 'standard', 'prettier'],
-  plugins: ['react-hooks'],
-  rules: {
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
-  },
+  extends: ['react-app', 'prettier'],
 };
